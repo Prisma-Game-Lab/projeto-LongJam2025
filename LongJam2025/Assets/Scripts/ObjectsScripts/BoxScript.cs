529655fc97670c3a299646e8d6d08d4fb3db3ef6
jomigmonte <jomigmonte@gmail.com>
2025-03-27 00:20:43 -0300

    apocalipse caixal
    muitas caixas épicas

---
+++ LongJam2025/Assets/Scripts/ObjectsScripts/BoxScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScript : ObjectManager
+{
+    [SerializeField] private GameObject item;
+
+    public void createBox()
+    {
+        Instantiate(item, new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 0), Quaternion.identity);
+    }
+}
